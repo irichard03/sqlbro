1196de0b119352480e5a98cd2e8ec44f52c431b1
Ian Richard <ianrichard03@gmail.com>
2019-12-10 22:49:04 -0600

    added SQL connection

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Data.SqlClient;
 
-
 namespace sqlbro
 {
     class Program
@@ -10,7 +9,45 @@ namespace sqlbro
         static void Main(string[] args)
         {
             ConfigurationHelper config = new ConfigurationHelper();
-            Console.WriteLine(config.getConfig("Storage"));            
+            string DatabaseConnection = config.getConfig("Database");
+
+            //connect to databse
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(DatabaseConnection))
+                {
+                    string query = @"SELECT @@VERSION";                
+                    SqlCommand command = new SqlCommand(query, connection);
+                    //open connection
+                    connection.Open();
+
+                    //execute the SQLCommand
+                    SqlDataReader dataReader = command.ExecuteReader();
+
+                    //check if there are records
+                    if (dataReader.HasRows)
+                    {
+                        while (dataReader.Read())
+                        {
+                            //display retrieved record (first column only/string value)
+                            Console.WriteLine(dataReader.GetString(0));
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found.");
+                    }
+                    dataReader.Close();
+                }
+            }
+            
+            catch (Exception ex)
+            {
+                //display error message
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+
+
         }
     }
 }
